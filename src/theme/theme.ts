0531eab05c5e7d89deb2ef1d5434b2903683609c
choihansu97 <jjpp9712@naver.com>
2023-11-24 17:48:57 +0900

    feat: theme 타입정의

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,13 +1,13 @@
-import {DefaultTeme} from 'styled-components';
+import {DefaultTheme} from 'styled-components';
 
-export const darkTheme: DefaultTeme = {
+export const darkTheme: DefaultTheme = {
     backgroundColor: "#232323",
     textColor: "#fff",
     subTextColor: "#f7f7f7",
     themeColor: "#ff6347",
 };
 
-export const lightTheme: DefaultTeme = {
+export const lightTheme: DefaultTheme = {
     backgroundColor: "#ffffff",
     textColor: "#04091a",
     subTextColor: "#04091a80",
