eefc90f89489a3c763314ff15b2eac9ba434eb7d
choihansu97 <jjpp9712@naver.com>
2023-11-24 18:01:40 +0900

    feat: API 작성

---
+++ src/apis/api.ts
@@ -0,0 +1,24 @@
+const BASE_URL = `https://api.coinpaprika.com/v1`;
+const CHART_URL = `https://ohlcv-api.nomadcoders.workers.dev`;
+
+const fetchCoinList = async () => {
+    const response = await fetch(`${BASE_URL}/coins`);
+    const data = response.json();
+    return data;
+};
+
+const getCoinPriceData = async (coinId) => {
+    const response = await fetch(`${BASE_URL}/tickers/${coinId}`);
+    const data = response.json();
+    return data;
+};
+
+const getCoinChartData = async (coinId) => {
+    const response = await fetch(`${CHART_URL}?coinId=${coinId}`);
+    const data = response.json();
+
+    if (!data) {
+        console.error('해당 데이터가 존재하지 않습니다.');
+    }
+    return data;
+};
