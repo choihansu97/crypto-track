7c6b86f0c4cb6922bba50a82dcaa13530f11da27
choihansu97 <jjpp9712@naver.com>
2023-11-24 17:43:45 +0900

    feat: theme 색상값 추가

---
+++ src/theme/theme.ts
@@ -0,0 +1,20 @@
+import {DefaultTeme} from 'styled-components';
+
+export const darkTheme: DefaultTeme = {
+    backgroundColor: "#232323",
+    textColor: "#fff",
+    subTextColor: "#f7f7f7",
+    themeColor: "#ff6347",
+};
+
+export const lightTheme: DefaultTeme = {
+    backgroundColor: "#ffffff",
+    textColor: "#04091a",
+    subTextColor: "#04091a80",
+    themeColor: "#ff6347",
+};
+
+export const theme = {
+    lightTheme,
+    darkTheme
+}
