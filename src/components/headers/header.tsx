064bf4c865764b0f0ff553762fda401771eeaf98
choihansu97 <60763027+choihansu97@users.noreply.github.com>
2023-11-25 19:57:06 +0900

    feat : 헤더 스타일 구성

--- src/components/headers/header.tsx
+++ src/components/headers/header.tsx
@@ -1,12 +1,14 @@
 import styled from "styled-components";
 import {useTheme} from "../../context/themeContext";
 import DarkModeToggle from "../darkModeToggle";
+import {Link} from 'react-router-dom';
 
 export default function Header() {
     const [ThemeMode, toggleTheme] = useTheme();
 
     return (
         <Wrapper>
+            <Logo><Link to={"/"}>CryptoTracker</Link></Logo>
             <DarkModeToggle toggle={toggleTheme} mode={ThemeMode}>
                 다크모드
             </DarkModeToggle>
@@ -14,6 +16,23 @@ export default function Header() {
     )
 }
 
-const Wrapper = styled.header `
-    display: flex;
+const Wrapper = styled.header`
+  display: flex;
+  align-items: center;
+  padding: 1.5rem 2rem;
+  border-bottom: 1px solid #dadada;
+`;
+
+const Logo = styled.h2`
+  font-size: 2rem;
+  font-weight: bold;
+
+  a {
+    color: cornflowerblue;
+    text-decoration: none;
+
+    &:hover {
+      opacity: 0.8;
+    }
+  }
 `;
