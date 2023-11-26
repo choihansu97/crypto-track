3c04dd7ed39a0094285a9960634a667c5450e841
choihansu97 <60763027+choihansu97@users.noreply.github.com>
2023-11-26 11:11:48 +0900

    feat : 뷰페이지 스타일 작성

--- src/pages/coinView.tsx
+++ src/pages/coinView.tsx
@@ -1,3 +1,5 @@
+import styled from "styled-components";
+
 export default function CoinView () {
     return (
         <>
@@ -5,3 +7,67 @@ export default function CoinView () {
         </>
     )
 }
+
+
+const Wrapper = styled.article`
+  display: flex;
+`;
+
+const Table = styled.table `
+  width: 100%;
+  font-size: 14px;
+  text-align: left;
+  
+   thead {
+     border-top: 1px solid #dadada;
+   }
+  
+  th { 
+    padding: 0.8rem;
+    background-color: white;
+    color: #121212;
+    font-weight: bold;
+  }
+  
+  td {
+    padding: 0.8rem;
+    color: #121212;
+    border-bottom: 1px solid #dadada
+  }
+`;
+
+const Links = styled.ul `
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0 0.5rem;
+  width: 100%;
+`;
+
+const TapWrapper = styled.div `
+  margin: 2rem;
+  article {
+    padding: 1rem;
+    border: 1px solid #dadada;
+    border-top: none;
+  }
+`;
+
+const TapMenu = styled.div `
+  a {
+    display: block;
+    flex: 1 1 50%;
+    padding: 0.7rem;
+    text-align: center;
+    border: 1px solid #093687;
+    font-size: 1.4rem;
+    transition: all 200ms ease-in-out;
+    background-color: white;
+
+    &:first-child {
+      border-right: 0 none;
+    }
+    &.active {
+      background-color: #093687;
+      color: #fff;
+    }
+`;
