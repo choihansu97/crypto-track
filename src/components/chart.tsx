8785ca2ce8e1b737f08a011982c611ff277602e3
choihansu97 <jjpp9712@naver.com>
2023-11-28 11:37:36 +0900

    feat: 차트 컴포넌트 추가

---
+++ src/components/chart.tsx
@@ -0,0 +1,89 @@
+import {useQuery} from "@tanstack/react-query";
+import {getCoinChartData} from "../apis/api";
+import Loading from "./loading";
+import ApexChart from 'react-apexcharts';
+import {darkTheme} from "../theme/theme";
+
+interface ChartProps {
+    coinId: string;
+};
+
+interface IHistorical {
+    time_open: string;
+    time_close: number;
+    open: number;
+    high: number;
+    low: number;
+    close: string;
+    volume: number;
+    market_cap: number;
+};
+
+export default function Chart({props}: ChartProps) {
+    const {isLoading, data} = useQuery<IHistorical[]>({
+        queryKey: ["ohlcv", 'btc-bitcoin'],
+        queryFn: () => getCoinChartData('btc-bitcoin'),
+        refetchInterval: 100000
+    });
+
+    return (
+        <>
+            {isLoading ?
+                <Loading/> :
+                <ApexChart
+                    type={"line"}
+                    series={[
+                        {
+                            name: "Price",
+                            data: data?.map(price => parseFloat(price.close)) ?? [],
+                        },
+                    ]}
+                    options={{
+                        theme: {mode: darkTheme ? "dark" : "light"},
+                        chart: {
+                            height: 500,
+                            width: 860,
+                            toolbar: {show: false},
+                            background: "transparent"
+                        },
+                        grid: {
+                            show: false,
+                        },
+                        yaxis: {
+                            show: false,
+                        },
+                        xaxis: {
+                            axisBorder: {show: false},
+                            axisTicks: {show: false},
+                            labels: {
+                                show: false,
+                            },
+                            type: "datetime",
+                            categories: data?.map(price =>
+                                new Date(price.time_close * 1000).toISOString()
+                            ),
+                        },
+                        stroke: {
+                            curve: "smooth",
+                            width: 4,
+                        },
+                        fill: {
+                            type: "gradient",
+                            gradient: {
+                                gradientToColors: ["#0be881"],
+                                stops: [0, 100],
+                            },
+                        },
+                        colors: ["#0fbcf9"],
+                        tooltip: {
+                            y: {
+                                formatter: value => `$${value.toFixed(2)}`,
+                            },
+                        },
+                    }
+                    }
+                />
+            }
+        </>
+    )
+}
