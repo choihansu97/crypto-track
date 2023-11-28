4ab9857efc6f34d21b060021b37c5be8f938a3e6
choihansu97 <jjpp9712@naver.com>
2023-11-28 10:55:56 +0900

    feat: 메인페이지 스타일 추가

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components";
 import {fetchCoinList} from "../apis/api";
 import Loading from "../components/loading";
 import {useQuery} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
 
 interface ICoin {
     id: string,
@@ -15,30 +16,28 @@ interface ICoin {
 }
 
 export default function Main() {
-    const { isLoading, data } = useQuery<ICoin>({ queryKey: ["allCoins"], queryFn: fetchCoinList });
+    const {isLoading, data} = useQuery<ICoin[]>({queryKey: ["allCoins"], queryFn: fetchCoinList});
 
     return (
         <Wrapper>
-            <Header>
-                <Title>타이틀입니다.</Title>
-            </Header>
-
-            {isLoading ? (
-                <Loading/>
-            ) : (
+            {isLoading ? <Loading/> :
                 <ContentList>
                     {data?.slice(0, 100).map((coin) => (
                         <Coin key={coin.id}>
                             <Link
                                 to={`/coin-view/${coin.id}`}
-                                state={coin}
+                                state={coin.id}
                             >
+                                <Img
+                                    src={`https://coinicons-api.vercel.app/api/icon/{coin.symbol.toLowerCase()}`}
+                                    alt={coin.name}/>
                                 {coin.name}
                             </Link>
                         </Coin>
                     ))}
                 </ContentList>
-            )}
+            }
+            <ReactQueryDevtools initialIsOpen={true}/>
         </Wrapper>
     );
 }
@@ -46,25 +45,43 @@ export default function Main() {
 const Wrapper = styled.article`
     display: flex;
     flex-direction: column;
-`;
-
-const Header = styled.header`
+    justify-content: center;
+    align-items: center;
+    max-width: 860px;
     width: 100%;
-    margin: 2rem 0;
-`;
-
-const Title = styled.h2`
-    color: #121212;
-    font-size: 32px;
-    font-weight: bold;
+    padding: 1rem;
+    margin: 0 auto;
 `;
 
 const ContentList = styled.ul`
     display: flex;
     flex-direction: column;
+    width: 100%;
 `;
 
 const Coin = styled.li`
-    padding: 1rem;
+    width: 100%;
     border: 1px solid #dadada;
+    border-radius: 16px;
+    background-color: white;
+
+    a {
+        display: flex;
+        align-items: center;
+        gap: 24px;
+        padding: 1rem;
+        border-radius: 16px;
+        font-size: 24px;
+        font-weight: 500;
+        text-decoration: none;
+
+        &:hover {
+            background-color: #f9f9f9;
+        }
+    }
 `;
+
+const Img = styled.img`
+    width: 64px;
+    height: 64px;
+`
